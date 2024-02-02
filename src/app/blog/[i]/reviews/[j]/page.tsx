aca6c319a981ee87c1059ca583f62aadf00d4d59
junaid1563 <junaidajmer87@gmail.com>
2024-02-03 01:25:33 +0530

    adding notFond page

--- src/app/blog/[i]/reviews/[j]/page.tsx
+++ src/app/blog/[i]/reviews/[j]/page.tsx
@@ -1,3 +1,8 @@
+import { notFound } from "next/navigation"
+
 export default function Review({ params }: { params: { i: string, j: string } }) {
+    if (parseInt(params.j) > 1000) { 
+        notFound();
+    }
     return <h1>Review {params.j} of product {params.i}</h1>
 }
