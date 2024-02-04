ce615f6f9ef40ecfccbe81638c3d27467273fcce
junaid1563 <junaidajmer87@gmail.com>
2024-02-04 13:47:59 +0530

    dynamic title

--- src/app/blog/[i]/reviews/[j]/page.tsx
+++ src/app/blog/[i]/reviews/[j]/page.tsx
@@ -2,15 +2,15 @@ import { notFound } from "next/navigation"
 import { Metadata } from "next";
 
 type Props = {
-    params: { i: string, j:string }
+    params: { i: string, j: string }
 }
 
 export function generateMetadata({ params }: Props): Metadata {
-    return { title: `Review ${params.j} of Blog ${params.i}` }
+    return { title: { absolute: `Review ${params.j} of Blog ${params.i}` } }
 }
 
 export default function Review({ params }: { params: { i: string, j: string } }) {
-    if (parseInt(params.j) > 1000) { 
+    if (parseInt(params.j) > 1000) {
         notFound();
     }
     return <h1>Review {params.j} of product {params.i}</h1>
